01922a8d84c836cac3ba71a94ed4b6461a374a38
crackpot1999hn <cr1999hn@gmail.com>
2022-01-19 22:23:18 +0330

    add project

---
+++ prj/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace prj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(359, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "اجرا";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(879, 317);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "مشتری";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "مدت بین دو ورود";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "زمان ورود";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "مدت خدمت دهی";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.HeaderText = "زمان شروع";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column6.HeaderText = "زمان پایان خدمت";
+            this.Column6.Name = "Column6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 431);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "سيستم صف فروشگاه";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+
+    }
+}
+
---
+++ prj/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+               int[][] a = new int[1000][];
+
+            for (int b = 0; b < 6; b++)
+            {
+
+                int r;
+
+                //مشتری
+                a[b] = new int[6] { 0, 0, 0, 0, 0 ,0};
+
+                a[b][0] = b + 1;
+
+                //مدت بین دو ورود
+                Random rand = new Random(b * 100);
+                r = rand.Next(0, 100);
+                if (b == 0)
+                {
+                    a[0][1] = 0;
+                }
+                else if (r >= 0 && r <= 30)
+                {
+                    a[b][1] = 1;
+                }
+                else if (r >= 31 && r <= 60)
+                {
+                    a[b][1] = 2;
+                }
+                else if (r >= 61 && r <= 80)
+                {
+                    a[b][1] = 3;
+                }
+                else if (r >= 81 && r <= 100)
+                {
+                    a[b][1] = 4;
+                }
+
+               
+
+                //زمان ورود
+                if (b == 0)
+                { //ورود اولین مشتری 
+                    a[0][2] = 0;
+                }
+                else
+                    a[b][2] = a[b][1] + a[b - 1][2];
+
+                //مدت خدمت دهی
+                int t;
+                Random rand1 = new Random(b * 1000);
+                t = rand1.Next(0, 50);
+                if (t >= 0 && t <= 10)
+                {
+                    a[b][3] = 1;
+                }
+                else if (t >= 21 && t <= 30)
+                {
+                    a[b][3] = 2;
+
+                }
+                else if (t >= 31 && t <= 40)
+                {
+                   a[b][3] = 3;
+                }
+                else if (t >= 41 && t <= 50)
+                {
+                    a[b][3] = 4;
+                }
+             
+
+             if (b == 0)
+             {
+
+                 a[0][4] = 0;
+                 a[0][3] = 2;
+                 a[0][5] = 2;
+             }
+             else if (b == 1)
+             {
+                 a[1][4] = 4;
+                 a[b][5] = a[b][4] + a[b][3];
+
+
+             }
+             else if (b == 2)
+             {
+                 a[2][4] = 5;
+                 a[b][5] = a[b][4] + a[b][3];
+             }
+
+             else if (b == 3)
+             {
+                 a[3][4] = 7;
+                 a[b][5] = a[b][4] + a[b][3];
+             }
+
+             else if (b == 4)
+             {
+                 a[4][4] = 10;
+                 a[b][5] = a[b][4] + a[b][3];
+             }
+
+             else
+             {
+                 a[5][4] = 14;
+                 a[b][5] = a[b][4] + a[b][3];
+             }
+
+                // if (a[b][4] > a[b][2])
+                //{
+                   
+                //    a[b][5] = a[b][4] + a[b][3];
+                  
+                //}
+               
+
+            }
+
+            for (int c = 0; c < 6; c++)
+            {
+                DataGridViewRow dgvRow = new DataGridViewRow();
+                dgvRow.CreateCells(dataGridView1);
+                object[] tem = new object[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    tem[i] = a[c][i];
+                }
+                dgvRow.SetValues(tem);
+                dataGridView1.Rows.Add(dgvRow);
+            }
+            
+        
+        }
+    }
+}
